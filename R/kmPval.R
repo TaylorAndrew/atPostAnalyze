913fcd86ecda265198ffafeb9fb12eee70ea961b
Andrew Taylor <taylor.andrew.r@gmail.com>
2017-02-15 11:59:41 -0500

    added digits argument

--- R/kmPval.R
+++ R/kmPval.R
@@ -3,6 +3,7 @@
 #' kmPval provides log-rank p-value provided in the survdiff() call.
 #'
 #' @param model is a survdiff() model object
+#' @param digits is the number of digits to round to
 #'
 #' @return pvalue shown in survdiff() model as a character string
 #'
@@ -12,8 +13,8 @@
 #'                 sample(c(0, 1), 100, replace = T, prob = c(.2, .8))
 #'                 ) ~ sample(c('no', 'yes'), 100, replace = T))
 #' kmPval(mod)
-kmPval <- function(model) {
+kmPval <- function(model, digits=3) {
   return(round(1 - pchisq(model$chisq,
                           length(model$n)-1),
-               digits = 3))
+               digits = digits))
 }
